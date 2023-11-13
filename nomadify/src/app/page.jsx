92a9f83cd65bf9f62b365efa1b69c012be6dbbf1
Adam Stádník <adam.stadnik@seznam.cz>
2023-11-13 21:19:08 +0100

    Added a responsive desing for the landing page

--- nomadify/src/app/page.jsx
+++ nomadify/src/app/page.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <section className='w-full flex-center flex-col pt-40 p-10 sm:p-40'>
+    <section className='w-full flex-center flex-col pt-40 p-10 pl-20 z-50'>
       <h1 className='head_text'>Pocket Guide to the <br /><span className='custom_font'>Prices</span> of Life.</h1>
       <button className='button text-center'>EXPLORE 🡫</button>
     </section>
