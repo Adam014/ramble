7d0c8eb161d7329abcdce58a303f36bf85302088
Adam Stádník <adam.stadnik@seznam.cz>
2023-11-14 18:40:53 +0100

    Added a description

--- nomadify/src/app/page.tsx
+++ nomadify/src/app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
     <section className='w-full flex-center flex-col pt-40 p-10 pl-20 z-50'>
       <h1 className='head_text'>Pocket Guide to the <br /><span className='custom_font'>Prices</span> of Life.</h1>
       <button className='button text-center'>EXPLORE 🡫</button>
+      <h2 className="text-3xl mt-20">Nomad<span className="custom_color">ify</span></h2>
+      <p className="w-96 text-xl mt-10">is your personal passport to the planet's <span className="custom_font">price tags</span>. It's not just a web app; it's a whisperer for your wallet. Real-time data, tailor-made recommendations, and a community of kindred travelers make Nomadify your go-to guide for worldly adventures without the financial fuss.</p>
     </section>
   )
 }
