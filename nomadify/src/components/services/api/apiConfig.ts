978534bd7ea75e19483a741088fd37107da1825a
Adam Stádník <adam.stadnik@seznam.cz>
2023-11-22 08:51:54 +0100

    Deleted the files for react redux, not gonna use it in Next

--- nomadify/src/components/services/api/apiConfig.ts
+++
@@ -1,3 +0,0 @@
-export const API_KEY = "d6850ca347mshb135b00f6e9f6b2p109ccejsnf924080e2e9b";
-export const API_HOST = 'tasty.p.rapidapi.com';
-export const API_BASE_URL = 'https://tasty.p.rapidapi.com';
