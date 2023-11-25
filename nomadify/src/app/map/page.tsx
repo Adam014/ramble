3231e063151ae8bb218c6524999efeccdf495707
Adam Stádník <adam.stadnik@seznam.cz>
2023-11-25 21:27:15 +0100

    Added back button

--- nomadify/src/app/map/page.tsx
+++ nomadify/src/app/map/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react';
 import MapChart from "@components/Mapchart";
 import { Tooltip as ReactTooltip } from "react-tooltip";
+import Link from 'next/link';
 
 const Map = () => {
     // state for the state onMouseOver
@@ -13,6 +14,7 @@ const Map = () => {
     return (
         <>  
             <h2 className="text-4xl m-10 mt-52 absolute">Choose your <span className="custom_font custom_color">country</span></h2>
+            <Link href="/" className='absolute right-0 mt-24 sm:mt-0 md:mt-10 m-10 cursor-pointer duration-200 hover:scale-125 active:scale-100'><p><span className='custom_font'>arrow</span> Return</p></Link>
             <div className='relative flex justify-center'>
                 <MapChart 
                     setTooltipCountry={setCountry}                    
