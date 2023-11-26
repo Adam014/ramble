4b95a4d76b149fe94da5c94d30165461cd2ec59b
Adam Stádník <adam.stadnik@seznam.cz>
2023-11-26 14:51:34 +0100

    Added a github star button

--- nomadify/src/components/Navbar.tsx
+++ nomadify/src/components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import Image from "next/image";
+import GitHubButton from 'react-github-btn'
 
 const Navbar = () => {
   return (
@@ -9,6 +10,9 @@ const Navbar = () => {
         <Link href="/" className="flex gap-2 flex-center p-10">
             <Image src="./assets/images/nomadify.svg" alt="nomadify-logo" width={200} height={200} className="object-contain relative z-50" />
         </Link>
+        <div className="fixed bottom-0 right-0 p-5">
+          <GitHubButton href="https://github.com/Adam014/Nomadify" data-size="large" data-show-count="true">Star this project on Github</GitHubButton>
+        </div>
     </div>
   )
 }
