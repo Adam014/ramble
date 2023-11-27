58f4aed5f0db75676042fe6eb5168eb34ce3d00b
Adam Stádník <adam.stadnik@seznam.cz>
2023-11-27 18:27:13 +0100

    Refactored the code

--- nomadify/src/app/map/[country]/page.tsx
+++ nomadify/src/app/map/[country]/page.tsx
@@ -1,16 +1,16 @@
 "use client"
 
-import React from 'react'
-import { useParams } from 'next/navigation'
+import React from 'react';
+import { useParams } from 'next/navigation';
 
 const singleRecipe = () => {
   const params = useParams();
-  console.log(params);
 
+  console.log(params);
   return (
-    <div>
-      Single State  	
-    </div>
+      <div>
+
+      </div>
   )
 }
 
