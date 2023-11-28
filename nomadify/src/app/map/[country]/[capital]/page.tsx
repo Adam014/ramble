8180624ed84898c15998b62559a5a6149568e5aa
Adam Stádník <adam.stadnik@seznam.cz>
2023-11-28 12:12:49 +0100

    Added decoding the url params values

--- nomadify/src/app/map/[country]/[capital]/page.tsx
+++ nomadify/src/app/map/[country]/[capital]/page.tsx
@@ -6,11 +6,17 @@ import { useParams } from 'next/navigation';
 const page = () => {
 
   const {country, capital} = useParams();
-  console.log(country, capital);
+
+  const decodeParam = (param) => Array.isArray(param) ? param.join(' ') : decodeURIComponent(param);
+  
+  const decodedCountry = decodeParam(country);
+  const decodedCapital = decodeParam(capital);
+
+  console.log(decodedCountry, decodedCapital);
 
   return (
     <div>
-      
+       <h1>{decodedCountry}, {decodedCapital}</h1>
     </div>
   )
 }
