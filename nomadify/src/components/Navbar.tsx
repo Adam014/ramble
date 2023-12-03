fc1eb1e5762c33b4c40511790dbea7748a7f5434
Adam Stádník <adam.stadnik@seznam.cz>
2023-12-03 22:21:34 +0100

    Refactored style

--- nomadify/src/components/Navbar.tsx
+++ nomadify/src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         <Link href="/" className="flex gap-2 flex-center p-10">
             <Image src="/assets/images/nomadify.svg" alt="nomadify-logo" width={200} height={200} className="object-contain relative z-50" />
         </Link>
-        <div className="fixed bottom-0 right-0 mb-56 mr-5 xl:mb-0 xl:mr-0 xl:p-10">
+        <div className="fixed bottom-0 right-0 mb-56 mr-5 xl:mb-0 xl:mr-0 xl:p-10 z-50">
           <GitHubButton href="https://github.com/Adam014/Nomadify" data-size="large" data-show-count="true">Star this project on Github</GitHubButton>
         </div>
     </div>
