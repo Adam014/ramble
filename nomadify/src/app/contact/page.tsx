218a7731df411222efb3fcc37719cdae6f357c86
Adam Stádník <adam.stadnik@seznam.cz>
2023-12-03 22:34:43 +0100

    Added a image to /contact

--- nomadify/src/app/contact/page.tsx
+++ nomadify/src/app/contact/page.tsx
@@ -1,15 +1,21 @@
-import React, { useEffect } from 'react'
+"use client";
+
+import React from 'react'
 import Image from 'next/image';
+import ScrollingImage from '@components/Scroll';
 
 const Contact = () => {
+  const { isImageFixed, scrollPosition } = ScrollingImage();
+
   return (
     <div className="relative">
       <Image 
         src="/assets/images/contact-animate.svg" 
-        height={500} 
-        width={500} 
+        height={600} 
+        width={600} 
         alt='contact-animation' 
-  
+        style={{ top: isImageFixed ? 0 : scrollPosition + 'px' }}
+        className={`contact-animation object-fit ${isImageFixed ? 'fixed' : 'absolute'} left-0 md:m-40`}
       />
     </div>
   )
