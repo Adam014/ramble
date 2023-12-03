7198b99bc0756c56dbf3558cc5524b6a13c079bd
Adam Stádník <adam.stadnik@seznam.cz>
2023-12-03 22:19:57 +0100

    Added image to /contact

--- nomadify/src/app/contact/page.tsx
+++ nomadify/src/app/contact/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Image from 'next/image';
 
 const Contact = () => {
   return (
-    <div>
-      Contact page
+    <div className="relative">
+      <Image 
+        src="/assets/images/contact-animate.svg" 
+        height={500} 
+        width={500} 
+        alt='contact-animation' 
+  
+      />
     </div>
   )
 }
