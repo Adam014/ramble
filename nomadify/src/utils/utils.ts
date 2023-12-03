2122ac2108522cd47edd1292a1b6be47a13c2ab7
Adam Stádník <adam.stadnik@seznam.cz>
2023-12-03 21:50:06 +0100

    Refactored utils.ts code for better readibility

--- nomadify/src/utils/utils.ts
+++ nomadify/src/utils/utils.ts
@@ -1,15 +1,26 @@
 import toast from 'react-hot-toast';
 
 const API_ENDPOINT = 'https://cost-of-living-and-prices.p.rapidapi.com/prices';
+const RAPIDAPI_KEY = process.env.NEXT_PUBLIC_RAPIDAPID_KEY;
+
+const ERROR_MESSAGES = {
+  RATE_LIMIT: 'API rate limit exceeded. Please try again later.',
+  GENERIC: 'API ERROR: The API is down, please be patient...',
+  NOT_FOUND: "API ERROR: We don't seem to have this city in our data!",
+};
 
 const fetchCostOfLiving = async (country: string, capital: string) => {
   const url = `${API_ENDPOINT}?city_name=${capital}&country_name=${country}`;
 
   try {
+    if (!RAPIDAPI_KEY) {
+      throw new Error('RapidAPI key is not defined.');
+    }
+
     const response = await fetch(url, {
       method: 'GET',
       headers: {
-        'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPIDAPID_KEY,
+        'X-RapidAPI-Key': RAPIDAPI_KEY,
         'X-RapidAPI-Host': 'cost-of-living-and-prices.p.rapidapi.com',
       },
     });
@@ -17,8 +28,8 @@ const fetchCostOfLiving = async (country: string, capital: string) => {
     if (!response.ok) {
       const errorMessage =
         response.status === 429
-          ? 'API rate limit exceeded. Please try again later.'
-          : 'API ERROR: The API is down, please be patient...';
+          ? ERROR_MESSAGES.RATE_LIMIT
+          : ERROR_MESSAGES.GENERIC;
 
       throw new Error(errorMessage);
     }
@@ -27,20 +38,19 @@ const fetchCostOfLiving = async (country: string, capital: string) => {
 
     if (responseData.error) {
       // API returned an error, handle it without saving to Supabase
-      toast.error("API ERROR: We don't seem to have this city in our data!");
-      throw new Error(`API Error: We dont seem to have this city in our data!${responseData.error.message}`);
+      toast.error(ERROR_MESSAGES.NOT_FOUND);
+      throw new Error(`API Error: We don't seem to have this city in our data! ${responseData.error.message}`);
     }
 
     return responseData;
   } catch (error) {
     // Handle any other errors during the API call
+    console.error('API Call Error:', error);
     throw new Error('Failed to fetch data from API');
   }
 };
 
-// function to refactore the date, for timezone, that is data originally fetched
+// function to refactor the date, for timezone, that is data originally fetched
 export const fixDate = (date: Date): Date => new Date(date.getTime() - date.getTimezoneOffset() * 60 * 1000);
-
-
 
 export default fetchCostOfLiving;
