c0b9cf3f164499ea3ce4291933f768c209f9ac95
Adam Stádník <adam.stadnik@seznam.cz>
2023-12-05 12:51:22 +0100

    Refactored code

--- nomadify/src/app/page.tsx
+++ nomadify/src/app/page.tsx
@@ -27,9 +27,6 @@ export default function Home() {
         </Link>
         <h2 className="text-5xl mt-40">Statistics</h2>
         <div className="sm:block md:flex gap-5">
-          {/* TODO: Add 4 statistics icons and numbers */}
-          {/* TODO: Show here the cost of living data, create a design for it in figma */}
-          {/* More than 200 countries, 54 subjects for Cost To Live, 17 currencies to view the Cost To Live, More than 8000 cities */}
           <div className="flex gap-5 items-center mt-20">
             <Image src="/assets/icons/globe_pink.png" height={50} width={50} alt="globe-icon"/>
             <h3 className="text-3xl"><CountUp end={200} duration={4}/>+ countries</h3>
