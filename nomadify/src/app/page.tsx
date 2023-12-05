c299489aa56e69742342b9cb77a062674f0058b6
Adam Stádník <adam.stadnik@seznam.cz>
2023-12-05 12:32:29 +0100

    Added the statistics to homepage

--- nomadify/src/app/page.tsx
+++ nomadify/src/app/page.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import ScrollingImage from '@components/Scroll';
 import Link from "next/link";
+import CountUp from 'react-countup';
 
 export default function Home() {
   // variables for image fixed and absolute 
@@ -24,12 +25,27 @@ export default function Home() {
         <Link href="/map">
           <button className='button text-center'>EXPLORE 🡢</button>
         </Link>
-        <h2 className="text-5xl mt-20"></h2>
-        <div className="flex gap-10">
+        <h2 className="text-5xl mt-40">Statistics</h2>
+        <div className="sm:block md:flex gap-5">
           {/* TODO: Add 4 statistics icons and numbers */}
           {/* TODO: Show here the cost of living data, create a design for it in figma */}
           {/* More than 200 countries, 54 subjects for Cost To Live, 17 currencies to view the Cost To Live, More than 8000 cities */}
-
+          <div className="flex gap-5 items-center mt-20">
+            <Image src="/assets/icons/globe_pink.png" height={50} width={50} alt="globe-icon"/>
+            <h3 className="text-3xl"><CountUp end={200} duration={4}/>+ countries</h3>
+          </div>
+          <div className="flex gap-5 items-center mt-20">
+            <Image src="/assets/icons/cost_pink.png" height={50} width={50} alt="cost-icon"/>
+            <h3 className="text-3xl"><CountUp end={53} duration={4}/>+ items/services</h3>
+          </div>
+          <div className="flex gap-5 items-center mt-20">
+            <Image src="/assets/icons/currency_pink.png" height={50} width={50} alt="currency-icon"/>
+            <h3 className="text-3xl"><CountUp end={17} duration={4}/>+ currencies</h3>
+          </div>
+          <div className="flex gap-5 items-center mt-20">
+            <Image src="/assets/icons/city_pink.png" height={50} width={50} alt="city-icon"/>
+            <h3 className="text-3xl"><CountUp end={8000} duration={4}/>+ cities</h3>
+          </div>
         </div>
       </section>
     </>
