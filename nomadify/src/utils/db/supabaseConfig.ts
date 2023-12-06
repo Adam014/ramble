ae5c3f69b9b259a8af5e241011cb4bcd4f2ea92e
Adam Stádník <adam.stadnik@seznam.cz>
2023-12-06 15:16:59 +0100

    Refactored the code

--- nomadify/src/utils/db/supabaseConfig.ts
+++ nomadify/src/utils/db/supabaseConfig.ts
@@ -1,16 +1,18 @@
 import { createClient } from '@supabase/supabase-js';
 
+// Declaring the url and key for the Supabase
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_APP_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+// Declaring the if statements if url or key are missing
 if (!supabaseUrl) {
   throw new Error('Supabase URL is missing in the environment variables');
-}
-
-if(!supabaseKey){
+} else if (!supabaseKey){
   throw new Error('Supabase Key is missing in the environment variables');
 }
 
+// declaring and creating the client for the supabase
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+// exporting the supabase
 export default supabase;
