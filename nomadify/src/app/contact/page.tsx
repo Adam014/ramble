0797af761bad6a8fc2528962bb7188123e9094fc
Adam Stádník <adam.stadnik@seznam.cz>
2023-12-07 00:00:54 +0100

    Added more inputs

--- nomadify/src/app/contact/page.tsx
+++ nomadify/src/app/contact/page.tsx
@@ -21,8 +21,24 @@ const Contact = () => {
       {/* TODO: Add here contact form */}
       <div className='absolute top-28 lg:left-1/5'>
         <h1 className='lg:text-6xl text-3xl relative lg:left-1/3 left-10'>Share <span className='custom_font custom_color'>ideas</span> with us!</h1>
-        <form>
-          <div className="relative lg:left-1/3 top-10">
+        <form className='relative lg:left-1/3 top-8 left-10'>
+          <div className="absolute mb-10 w-full">
+            <input
+                type="email"
+                placeholder="First name"
+                className="w-full pl-4 pr-3 py-2 text-white bg-transparent outline-none border-b-2 shadow-sm"
+                required
+            />
+          </div><br /><br /><br />
+          <div className="relative mb-10">
+            <input
+                type="text"
+                placeholder="Last name"
+                className="w-full pl-4 pr-3 py-2 text-white bg-transparent outline-none border-b-2 shadow-sm"
+                required
+            />
+          </div>
+          <div className="relative">
             <svg className="w-6 h-6 text-gray-400 absolute left-3 inset-y-0 my-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
             </svg>
@@ -33,6 +49,7 @@ const Contact = () => {
                 required
             />
           </div>
+          <button>Send</button>
         </form>
       </div>
     </div>
