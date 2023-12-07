84445a3d3be019f6713e25a79d1744e60bcab8d0
Adam Stádník <adam.stadnik@seznam.cz>
2023-12-07 23:35:24 +0100

    Refactored the homepage

--- nomadify/src/app/page.tsx
+++ nomadify/src/app/page.tsx
@@ -25,8 +25,8 @@ export default function Home() {
           <button className='button text-center'>EXPLORE 🡢</button>
         </Link>
         <h2 className="text-5xl mt-28">Unleash Your Inner Nomad</h2>
-        <p className="sm:w-11/12 md:w-1/2 pt-5">Deciding to explore the earth shouldn’t cost the earth! Can you afford that splendid solitude in Siberia or the charming chateaus of France? We’re here to turn your dreams into reality. With Nomadify, you're just a click away to know your cost.</p>
-        <div className="sm:block lg:flex gap-20">
+        <p className="sm:w-11/12 md:w-1/2 pt-2">Deciding to explore the earth shouldn’t cost the earth! Can you afford that splendid solitude in Siberia or the charming chateaus of France? We’re here to turn your dreams into reality. With Nomadify, you're just a click away to know your cost.</p>
+        <div className="sm:block lg:flex gap-20 -mt-10">
           {/* Custom styled components for the counting up animation, better for overusage */}
           <StatsImage icon="/assets/icons/globe_pink.png" count={200} label="countries" />
           <StatsImage icon="/assets/icons/cost_pink.png" count={53} label="items/services" />
