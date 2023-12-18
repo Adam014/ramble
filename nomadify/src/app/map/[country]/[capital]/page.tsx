5590c586f5cc5d213026aa833d795f1d4e5858ba
Adam Stádník <adam.stadnik@seznam.cz>
2023-12-18 19:07:23 +0100

    Added custom loader

--- nomadify/src/app/map/[country]/[capital]/page.tsx
+++ nomadify/src/app/map/[country]/[capital]/page.tsx
@@ -38,7 +38,10 @@ const Page = () => {
       <div className="heading_container pl-10 md:p-10 lg:pl-24 pt-24">
         <h1 className='head_text'>{decodedCountry}, {decodedCapital}</h1>
         {/* TODO: Custom loader */}
-        {loading && <p className='p-24'>Loading...</p>}
+        {loading && <p className='text-2xl p-32 flex items-center'> <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      </svg>Loading...</p>}
         {error && <p className='p-24'>{error}</p>}
         {costOfLivingData ? (
           <>
