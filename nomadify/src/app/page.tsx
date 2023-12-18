837c0add8f2aa6174db30e2d05d605a44ed5fd7a
Adam Stádník <adam.stadnik@seznam.cz>
2023-12-18 19:04:02 +0100

    Styling...

--- nomadify/src/app/page.tsx
+++ nomadify/src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         </Link>
         <h2 className="text-5xl mt-28 z-50 relative">Unleash Your Inner Nomad</h2>
         <p className="sm:w-11/12 md:w-1/2 pt-2">Deciding to explore the earth shouldn’t cost the earth! Can you afford that splendid solitude in Siberia or the charming chateaus of France? We’re here to turn your dreams into reality. With Nomadify, you're just a click away to know your cost.</p>
-        <div className="sm:block lg:flex gap-20 -mt-10">
+        <div className="sm:block lg:flex flex-wrap gap-20 -mt-10">
           {/* Custom styled components for the counting up animation, better for overusage */}
           <StatsImage icon="/assets/icons/globe_pink.png" count={200} label="countries" />
           <StatsImage icon="/assets/icons/cost_pink.png" count={53} label="items/services" />
@@ -28,6 +28,7 @@ export default function Home() {
           <StatsImage icon="/assets/icons/city_pink.png" count={8000} label="cities" />
         </div>  	
 
+        {/* TODO: Add more content to the homepage */}
       </section>
     </>
   )
