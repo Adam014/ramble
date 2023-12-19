7d5c3f3425cf228269a8c9b5b27cf8938ed63148
Adam Stádník <adam.stadnik@seznam.cz>
2023-12-19 21:00:08 +0100

    Fixed the grid bug with the animations

--- nomadify/src/app/page.tsx
+++ nomadify/src/app/page.tsx
@@ -22,7 +22,7 @@
           <h2 className="text-5xl mt-28 z-50 relative">Unleash Your Inner Nomad</h2>
           <p className="sm:w-11/12 md:w-1/2 pt-2">Deciding to explore the earth shouldn’t cost the earth! Can you afford that splendid solitude in Siberia or the charming chateaus of France? We’re here to turn your dreams into reality. With Nomadify, you're just a click away to know your cost.</p>
           
-          <div className="sm:block lg:flex flex-wrap">
+          <div className="sm:block md:flex flex-wrap">
             <StatsImage icon="/assets/icons/globe_pink.png" count={200} count_start={132} label="countries" />
             <StatsImage icon="/assets/icons/cost_pink.png" count={53} count_start={23} label="items/services" />
             <StatsImage icon="/assets/icons/currency_pink.png" count={17} count_start={4} label="currencies" />
--- nomadify/src/components/StatsImage.tsx
+++ nomadify/src/components/StatsImage.tsx
@@ -11,11 +11,11 @@ interface StatsImageProps {
 }
 
 const StatsImage: React.FC<StatsImageProps> = ({ icon, count, count_start, label }) => (
-  <div className='flex w-1/3'>
+  <div className='flex w-96'>
     <div className="flex items-center mt-12">
       <Image src={icon} height={80} width={80} alt={`${label}-icon`} />
       <div>
-        <h3 className='text-5xl ml-16'>
+        <h3 className='text-5xl ml-12'>
           +<CountUp isCounting start={count_start} end={count} duration={5} thousandsSeparator=','/>
         </h3>
         <p className='ml-16'>{label}</p>
