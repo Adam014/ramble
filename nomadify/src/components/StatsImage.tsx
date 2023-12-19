3a9b4ce734fc634a2d44dec139034c8bebed00ab
Adam Stádník <adam.stadnik@seznam.cz>
2023-12-19 20:29:34 +0100

    Fixed bug with the countup animation

--- nomadify/src/components/StatsImage.tsx
+++ nomadify/src/components/StatsImage.tsx
@@ -1,19 +1,26 @@
+// StatsImage.tsx
 import React from 'react';
 import Image from 'next/image';
-import CountUp from 'react-countup';
+import { CountUp } from 'use-count-up';
 
 interface StatsImageProps {
   icon: string;
   count: number;
+  count_start: number;
   label: string;
 }
 
-const StatsImage: React.FC<StatsImageProps> = ({ icon, count, label }) => (
-  <div className="flex gap-5 items-center mt-20">
-    <Image src={icon} height={50} width={50} alt={`${label}-icon`} />
-    <h3 className="text-3xl">
-      <CountUp end={count} duration={4} />+ {label}
-    </h3>
+const StatsImage: React.FC<StatsImageProps> = ({ icon, count, count_start, label }) => (
+  <div className='flex w-1/3'>
+    <div className="flex items-center mt-12">
+      <Image src={icon} height={80} width={80} alt={`${label}-icon`} />
+      <div>
+        <h3 className='text-5xl ml-16'>
+          +<CountUp isCounting start={count_start} end={count} duration={5} thousandsSeparator=','/>
+        </h3>
+        <p className='ml-16'>{label}</p>
+      </div>
+    </div>
   </div>
 );
 
