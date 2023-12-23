b55506a421d9424796b4bb0959d18a067644f406
Adam Stádník <adam.stadnik@seznam.cz>
2023-12-23 23:18:26 +0100

    Refactored ItemCard

--- nomadify/src/components/ItemCard.tsx
+++ nomadify/src/components/ItemCard.tsx
@@ -1,43 +1,42 @@
 import React, { FC, useMemo } from "react";
-import { getConvertedPrice } from "@utils/utils";
+// import { getConvertedPrice } from "@utils/utils";
 
 interface Item {
-    good_id: number;
-    avg: number;
-    max: number;
-    min: number;
-    measure: string;
-    item_name: string;
-    category_name: string;
-    currency_code: string;
+  good_id: number;
+  avg: number;
+  max: number;
+  min: number;
+  measure: string;
+  item_name: string;
+  category_name: string;
+  currency_code: string;
 }
 
 interface ItemProps {
-    data: Item[],
+  data: Item[];
 }
 
 const ItemCard: FC<ItemProps> = ({ data }) => {
-    const memoizedElements = useMemo(
-        () =>
-            data?.map((item) => (
-                // TODO: Refactor this design by the design in figma
-                <div key={item.good_id}>
-                    <h2 className="mt-2 sm:text-xl lg:text-3xl">{item.item_name}</h2>
-                    <div className="sm:block md:flex gap-5 mb-2 mt-2">
-                        <p>Maximum price: <span className="custom_color">$ {item.max}</span></p>
-                        <p>Average price: <span className="custom_color">$ {item.avg}</span></p>
-                        <p>Minimum price: <span className="custom_color">$ {item.min}</span></p>
-                    </div>
-                    <div className="flex justify-end mb-2">
-                        <h3 className="custom_font">{item.category_name}</h3>
-                    </div>
-                    <hr />
-                </div>
-            )),
-        [data]
-    );
+  const memoizedElements = useMemo(
+    () =>
+      data.map((item) => (
+        <React.Fragment key={item.good_id}>
+          <h2 className="mt-2 sm:text-xl lg:text-3xl">{item.item_name}</h2>
+          <div className="sm:block md:flex gap-5 mb-2 mt-2">
+            <p>Maximum price: <span className="custom_color">$ {item.max}</span></p>
+            <p>Average price: <span className="custom_color">$ {item.avg}</span></p>
+            <p>Minimum price: <span className="custom_color">$ {item.min}</span></p>
+          </div>
+          <div className="flex justify-end mb-2">
+            <h3 className="custom_font">{item.category_name}</h3>
+          </div>
+          <hr />
+        </React.Fragment>
+      )),
+    [data]
+  );
 
-    return <>{memoizedElements}</>;
+  return <>{memoizedElements}</>;
 };
 
 export default ItemCard;
