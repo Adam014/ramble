44762d71ed1f39b13ed34d5c80f9121ada005da8
Adam Stádník <adam.stadnik@seznam.cz>
2024-02-02 17:26:00 +0100

    Fixed bug with Link in Navbar

--- nomadify/src/components/Navbar.tsx
+++ nomadify/src/components/Navbar.tsx
@@ -6,9 +6,11 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <div className="nav">
-        <Link href="/" className="flex gap-2 flex-center p-10">
-            <Image src="/assets/images/nomadify.svg" alt="nomadify-logo" width={200} height={200} className="object-contain relative z-50" />
-        </Link>
+        <div className="flex gap-2 flex-center p-10">
+            <Link href="/"> 
+              <Image src="/assets/images/nomadify.svg" alt="nomadify-logo" width={200} height={200} className="object-contain relative z-50" />
+            </Link>
+        </div>
     </div>
   )
 }
