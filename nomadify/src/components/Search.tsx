07527b9a2a89546ca7360c4273c7dc04f7f7e581
Adam Stádník <adam.stadnik@seznam.cz>
2024-03-21 16:40:32 +0100

    Added working search input and changing placeholder

--- nomadify/src/components/Search.tsx
+++ nomadify/src/components/Search.tsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useRouter } from "next/navigation";
+import mapData from '../../public/map.json';
 
 const Search = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [placeholder, setPlaceholder] = useState("E.g., Czech Republic, Prague");
+  const [showPlaceholder, setShowPlaceholder] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    // Function to update placeholder every 15 seconds
+    const intervalId = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * mapData.objects.world.geometries.length);
+      const randomCountry = mapData.objects.world.geometries[randomIndex].properties.name;
+      const randomCapital = mapData.objects.world.geometries[randomIndex].properties.capital;
+      
+      setShowPlaceholder(false);
+      setTimeout(() => {
+        setPlaceholder(`E.g., ${randomCountry}, ${randomCapital}`);
+        setShowPlaceholder(true);
+      }, 300); // Delay placeholder change to sync with CSS animation
+    }, 15000); // Change placeholder every 15 seconds
+
+    return () => {
+      clearInterval(intervalId); // Cleanup function to clear interval when component unmounts
+    };
+  }, []);
+
+  const handleInputChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    const [country, capital] = searchValue.split(',').map(item => item.trim());
+    if (country && capital) {
+      router.push(`/map/${country}/${capital}`);
+    }
+  };
+
   return (
     <div className="ml-10 mb-10">
-        <input type="text" name="text" className="input w-1/5" placeholder="Search city..." />
+      <input
+        type="text"
+        className="input w-1/5"
+        value={searchValue}
+        placeholder={placeholder}
+        onChange={handleInputChange}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            handleSubmit();
+          }
+        }}
+      />
+      <style jsx>{`
+        input::placeholder {
+          opacity: ${showPlaceholder ? 1 : 0};
+          transition: opacity 0.3s ease-in-out;
+        }
+      `}</style>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
