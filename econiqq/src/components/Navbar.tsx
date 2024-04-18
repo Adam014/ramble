868dae33854245eb3596c774864eaecff5de3e57
adam.stadnik <adam.stadnik@displio.eu>
2024-04-19 01:10:25 +0200

    Adding new logo in the navbar

--- econiqq/src/components/Navbar.tsx
+++ econiqq/src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div className="nav">
         <div className="flex gap-2 flex-center p-10">
             <Link href="/"> 
-              <Image src="/assets/images/nomadify.svg" alt="nomadify-logo" width={200} height={200} className="object-contain relative z-50 nomadify-logo" />
+              <Image src="/assets/images/nomadify.png" alt="nomadify-logo" width={100} height={100} className="object-contain relative z-50 nomadify-logo" />
             </Link>
         </div>
     </div>
