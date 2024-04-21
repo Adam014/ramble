c6deb759cdf836fc766a627812d8f6ad85699774
Adam014 <adam.stadnik@seznam.cz>
2024-04-21 12:54:01 +0200

    Deleting unused components + refactoring

--- econiqq/src/components/CityCard.tsx
+++ econiqq/src/components/CityCard.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 const CityCard = (city, index: number) => {
+    console.log(city)
+    const singleCity = city.city
     return (
         <div className="city-card-container cursor-pointer" key={index}>
             <div className="city-info">
-                <h1 className="city-name">{city.city.city}</h1>
-                <h2 className="country-name">{city.city.country}</h2>
+                <h1 className="city-name">{singleCity.city}</h1>
+                <h2 className="country-name">{singleCity.country}</h2>
             </div>
             <div className="city-image-container">
                 <div className="overlay"></div>
-                <img src={city.city.data.image} className="city-image" alt="city" />
+                <img src={singleCity.data.image} className="city-image" alt="city" />
             </div>
         </div>
     );
