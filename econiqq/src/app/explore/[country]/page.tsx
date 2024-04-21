ffe2c2866f713b77ac5fe636d3a4ed8486fe6cd4
adam.stadnik <adam.stadnik@displio.eu>
2024-04-22 01:09:46 +0200

    fix: resolving error build

--- econiqq/src/app/explore/[country]/page.tsx
+++ econiqq/src/app/explore/[country]/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { ArrowBack } from '@components/ArrowBack';
 import { NoCity } from '@components/NoCity';
 import React from 'react';
 
@@ -11,9 +10,6 @@ import React from 'react';
 const country = () => {
   return (
     <>    
-      <div className='absolute top-0 right-0 mr-50 m-10 back-button'>
-          <ArrowBack />
-      </div>
       <div className='absolute top-1/3'>
         <NoCity />
       </div>
