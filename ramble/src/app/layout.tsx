aaacc16f2949a33c427d181d36cc5b1f766dbbb2
adam.stadnik <adam.stadnik@seznam.cz>
2024-08-09 00:25:13 +0200

    everlong

--- ramble/src/app/layout.tsx
+++ ramble/src/app/layout.tsx
@@ -3,6 +3,7 @@ import '@styles/globals.css'
 import Navbar from '@components/Navbar'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import Footer from '@components/Footer'
 
 export const metadata = {
   title: 'Ramble',
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
         <main className="app">
           <Navbar />
           {children}
+          <Footer />
         </main>
 
         <Analytics />
