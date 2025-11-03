f89417d9da7eb385aa01b0b00ade0e722e973471
Adam014 <adam.stadnik@seznam.cz>
2025-11-03 14:29:13 +0100

    final touches

--- ramble/src/app/keep-alive/route.js
+++ ramble/src/app/keep-alive/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export const dynamic = 'force-dynamic';
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_APP_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
